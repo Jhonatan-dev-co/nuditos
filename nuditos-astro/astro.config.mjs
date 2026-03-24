01367c4b6ac7dbeafd87cfe4e6963c93fcc57202
Jhonatan-dev-co <jhonatanjr1212@gmail.com>
2026-03-23 23:48:35 -0500

    fix: quitar sitemap temporalmente

--- nuditos-astro/astro.config.mjs
+++ nuditos-astro/astro.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://nuditos.com.co',
   output: 'hybrid',
   adapter: netlify(),
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date('2025-03-23'),
-    }),
-  ],
   vite: {
     css: {
       devSourcemap: true,
